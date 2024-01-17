a78e39b4ab9fe2ada8b5a22ab0b44e4f5779b00e
Dan Brook <dan@broquaint.com>
2024-01-17 09:07:36 +0700

    Implement Range support for the logfile server
    This allows "tailing" the logfile e.g
    
        $ tail -c $((1024 * 8)) cao-logfile29 | wc -l
              14
        $ curl -s -H "Range: bytes=$((8223775 - 1024 * 8))-8223775" http://localhost:8008/ | wc -l
              14
    
    An initial HEAD request is needed to fetch the Content-Length to
    figure out the range but such is the way of HTTP.

--- logfile-server.ts
+++ logfile-server.ts
@@ -1,10 +1,19 @@
-import { Application } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { Application, Context, Status } from "https://deno.land/x/oak@v12.6.1/mod.ts";
 
 const app = new Application();
-app.use(async(ctx) => {
+app.use(async(ctx: Context) => {
     // Reading it every time isn't optimal but that isn't germane to our purposes.
-    const logfile = await Deno.readFile("./webzook-0.30.logfile");
-    ctx.response.body = logfile;
+    const logfile = await Deno.readFile("./webzook-0.30.logfile");;
+    if(ctx.request.headers.has('range')) {
+        // Ok range could be garbage but this isn't exposed to The Internet.
+        const [_, fromStr, toStr] = ctx.request.headers.get('range')!.match(/\w+=(\d+)-(\d+)/)!
+        const [from, to] = [Math.max(0, Number(fromStr)), Math.min(logfile.length, Number(toStr))]
+        ctx.response.body = logfile.slice(from, to)
+        ctx.response.status = Status.PartialContent
+    }
+    else {
+        ctx.response.body = logfile
+    }
 });
 
 await app.listen({ port: 8008 });
